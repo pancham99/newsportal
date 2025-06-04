cca751010d33151b8afbeb907bbfbcdc4fa1fd7c
pancham99 <pancham.047@gmail.com>
2025-06-04 13:44:15 +0530

    add

--- components/Header.jsx
+++ components/Header.jsx
@@ -17,7 +17,7 @@ const Header = () => {
         <div>
             <div className='px-2 lg:px-8 flex justify-between items-center bg-[#333333] text-[#cccccc]'>
                 {/* <span className='text-sm'>{moment().local().format('LLLL')}</span> */}
-                {moment().tz("Asia/Kolkata").format('LLLL')}
+                <span className='text-sm'>{moment().tz("Asia/Kolkata").format('LLLL')}</span>
                 <div className='flex gap-2'>
                     <Link className='flex justify-center items-center bg-[#ffffff2b] p-2' target="_blank" href={"https://www.facebook.com/people/Top-Briefing/61552965021716/?rdid=GXwcwyboiwHXnvQF&share_url=https%3A%2F%2Fwww.facebook.com%2Fshare%2F18trThXJnu%2F"}><FaFacebookF className="text-white flex justify-center items-center " /></Link>
                     <Link className='flex justify-center items-center bg-[#ffffff2b] p-2' target="_blank" href={"https://www.instagram.com/topbriefing/?igsh=ZXJ5ZTdrbm9ycTJl#"}><FaInstagram className="text-white flex justify-center items-center " /></Link>
