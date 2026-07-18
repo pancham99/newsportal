c8da64857128784dd94dfd87e83ede4edb96c761
keshrirai505 <keshrirai505@gmail.com>
2026-07-18 12:01:20 +0530

    save

--- config/config.js
+++ config/config.js
@@ -1,8 +1,8 @@
 const production = 'production';
 const local = 'dev';
 
-const local_api_url = 'https://bakendtopbrefing.vercel.app/api';  
-const production_api_url = 'https://bakendtopbrefing.vercel.app/api';
+const local_api_url = 'http://localhost:5001';  
+// const production_api_url = 'https://bakendtopbrefing.vercel.app';
 
 // const local_api_url = 'http://localhost:5001';  
 // const production_api_url = 'http://localhost:5001';
