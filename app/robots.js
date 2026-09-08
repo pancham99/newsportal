d23ff096663ef3849cc0c28a681a3ce55fec97c2
pancham99 <pancham.047@gmail.com>
2026-09-08 23:24:42 +0530

    fix chatgpt

--- app/robots.js
+++ app/robots.js
@@ -3,10 +3,16 @@ import { SITE_URL } from "../config/site";
 export default function robots() {
   return {
     
-    rules: {
-      userAgent: "*",
-      allow: "/",
-    },
+     rules: [
+      {
+        userAgent: "OAI-SearchBot",
+        allow: "/",
+      },
+      {
+        userAgent: "*",
+        allow: "/",
+      },
+    ],
     host: SITE_URL.replace(/^https?:\/\//, ""),
     sitemap: [
       `${SITE_URL}/sitemap.xml`,
